3506cebb9a62754f36017890cbdd12b4294d3370
stricker <liuchen195@163.com>
2016-07-11 14:12:21 +0800

    Initial Commit

---
+++ iOS_youbiSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_youbiSystem
+//
+//  Created by 刘琛 on 16/7/11.
+//  Copyright © 2016年 cyjd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_youbiSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_youbiSystem
+//
+//  Created by 刘琛 on 16/7/11.
+//  Copyright © 2016年 cyjd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
